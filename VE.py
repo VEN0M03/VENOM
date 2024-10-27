5f1c67bd3bab5ebda28b7d9696d4df836418577a
VEN0M03 <pythonvenom291@gmail.com>
2024-10-27 09:17:18 +0100

    Update VE.py

--- VE.py
+++ VE.py
@@ -1,7 +1,9 @@
 import os, sys
 try:os.system('git pull')
 except:pass
+try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
+except:pass
 try:
-    __import__("VE")._____VENOM_()
+    __import__("VENOM")._____niki_()
 except Exception as e:
     exit(str(e))
