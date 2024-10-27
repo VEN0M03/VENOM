5aff61cae13ccca2d9e5db732bf8a065acf0b9cd
VEN0M03 <pythonvenom291@gmail.com>
2024-10-27 09:18:48 +0100

    Update VE.py

--- VE.py
+++ VE.py
@@ -4,6 +4,6 @@ except:pass
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("VENOM")._____niki_()
+    __import__("VE")._____niki_()
 except Exception as e:
     exit(str(e))
